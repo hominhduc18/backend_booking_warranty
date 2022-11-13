cc883346687e207110c1126662d699b559123927
nguyentran <nt85272@gmail.com>
2022-11-13 11:32:00 +0700

    update attribute

--- Models/employee.js
+++ Models/employee.js
@@ -1,62 +1,68 @@
-const mongoose = require('mongoose');
-const employeeSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        minLength: 6,
-        maxLength: 25,
-        unique: true  
+const mongoose = require("mongoose");
+const employeeSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 25,
+      unique: true,
     },
-    email:{
-        type: String,
-        required: true,
-        minLength: 6,
-        maxLength: 25,
-        unique: true 
-    }, 
-    password:{
-        type: String,
-        required: true,
-        minLength: 6,
+    email: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 25,
+      unique: true,
     },
-    
-    phone:{
-        type: Number,
-        minLength:1,
-        maxLength:12,
-        unique: true,
-        required: true
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
     },
-    sex:{
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 10,
+
+    phone: {
+      type: Number,
+      minLength: 1,
+      maxLength: 12,
+      unique: true,
+      required: true,
+    },
+    sex: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 10,
     },
     history: {
-        experience: {
-            type: String,
-            required: true,
-            minLength: 5,
-            maxLength: 20,
+      experience: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 20,
+      },
+      start_avg: {
+        type: Number,
+        required: true,
+        minLength: 5,
+        maxLength: 20,
+      },
+      maintenance: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Maintenance",
+      },
+      location: {
+        latitude: {
+            type: Number
         },
-        start_avg: {
-            type: Number,
-            required: true,
-            minLength: 5,
-            maxLength: 20,
-        },
-        maintenance: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref : "Maintenance",
-        },
-        
-
+        longitude: {
+            type: Number
+        }
+      },
     },
-},
-// thời gian update
-{timestamps: true}
+  },
+  // thời gian update
+  { timestamps: true }
 );
-let Employee = mongoose.model('Employee',employeeSchema );
+let Employee = mongoose.model("Employee", employeeSchema);
 module.exports = Employee;
-
