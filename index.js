7274de0378407539af9b5cf82d13504ec2154922
duchm <duchm@tecacom.intern>
2022-12-31 10:08:26 +0700

    main

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ mongoose.connect(process.env.URL,()=>{
     console.log("connect started");
 })
    
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => { res.send("HOME");});
 
