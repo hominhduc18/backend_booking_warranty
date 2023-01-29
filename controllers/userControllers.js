0f5e6ba89036753446bd2d56cb905518fc4472c9
hominhduc18 <hoduc092@gmail.com>
2023-01-29 22:26:52 +0700

    main

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -48,6 +48,7 @@ const userControllers = {
     all_Booking_service: async(req, res) =>{
         try {
             const user = await User.find({_id: req.body.id}).populate('maintenance_id');
+            res.send(user);
             res.status(200).json(user);
         } catch (error) {
             res.status(500).json(error);
