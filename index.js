f147673eeb1de9a4408235b1ca3018730360863e
hominhduc18 <hoduc092@gmail.com>
2023-02-13 22:02:24 +0700

    hihi

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ app.use(express.json());
 //   });
 // });
 
-// app.use(
-//     express.urlencoded({
-//         extended: true,
-//     }),
-// );
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 
 
 // mongoose.connect("mongodb://localhost:27017/DATN", (err) => {
