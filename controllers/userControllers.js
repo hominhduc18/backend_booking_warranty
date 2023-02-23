55030b87c0f7808d24a168c4612b12bccc92a0cf
hominhduc18 <hoduc092@gmail.com>
2023-02-24 01:32:38 +0700

    main

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -79,7 +79,9 @@ const userControllers = {
     getUser: async (req, res) => {
         try {
             const user = await User.findById(req.params.id );
-            res.status(200).json(user);
+            const {password, ...other} = user._doc;
+            res.status(200).json({...other, user});
+           
         } catch (error) {
             res.status(500).json(error)
         }
