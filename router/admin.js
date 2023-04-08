d779e829c9320f6c2396edb70ed9d8de613fdaf6
hominhduc18 <hoduc092@gmail.com>
2023-04-08 11:27:33 +0700

    DOne

--- router/admin.js
+++ router/admin.js
@@ -1,16 +1,14 @@
-const adminController = require("../controllers/adminControllers")
-const middlewareControllers = require("../middleware/middlewareControllers")
+
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const { Router } = require("express");
-const { model } = require("mongoose");
+
 const adminControllers = require("../controllers/adminControllers");
-const { route } = require("./user");
 
 
-router.post('/registerAdmin', adminController.register_admin);
 
-router.post('/loginAdmin', adminController.loginAdmin);
+router.post('/registerAdmin', adminControllers.register_admin);
+
+router.post('/loginAdmin', adminControllers.loginAdmin);
 
 router.post('/logoutAdmin',middlewareControllers.verifyToken, adminControllers.logoutAdmin);
 
